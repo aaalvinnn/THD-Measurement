a7631507feb66f16cc951b3f194a26b467ebb663
aaalvinnn <2869242945@qq.com>
2023-04-15 16:13:41 +0800

    降低单片机频率以降低采样频率（1/8）

--- Inc/tim.h
+++ Inc/tim.h
@@ -34,11 +34,14 @@ extern "C" {
 
 extern TIM_HandleTypeDef htim3;
 
+extern TIM_HandleTypeDef htim8;
+
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
 
 void MX_TIM3_Init(void);
+void MX_TIM8_Init(void);
 
 /* USER CODE BEGIN Prototypes */
 
